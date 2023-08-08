0b0ccec2beccd3b69c61adae01e6c3f6f6cdcfdc
KasumyanRafael <100214646+KasumyanRafael@users.noreply.github.com>
2023-08-08 15:05:57 +0300

    Создал окошко, где будет информация об оставшихся карточках и времени

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 var button=document.querySelector("#play");
 button.addEventListener("click",function(){
-    let firstname=document.querySelector("#firstbox").value;
-    let secondname=document.querySelector("#secondbox").value;
+    let firstname=document.querySelector("#firstbox");
+    let secondname=document.querySelector("#secondbox");
     let firstgamer=document.querySelector("#firstgamername");
     let secondgamer=document.querySelector("#secondgamername");
-    if(firstname!=" ")
-    {
-        firstgamer.value=firstname;
+    let firstscore=document.querySelector("#firstscore");
+    let secondscore=document.querySelector("#secondscore");
+    if(firstname.value===""){
+        firstgamer.textContent="Первый игрок";
     }
     else{
-        firstgamer.value="Первый игрок";
+        firstgamer.textContent=firstname.value;
     }
-    if(secondname!=" ")
-    {
-        secondgamer.value=secondname;
+    if(secondname.value===""){
+        secondgamer.textContent="Второй игрок";
     }
     else{
-        secondgamer.value="Второй игрок";
+        secondgamer.textContent=secondname.value;
     }
+    firstscore.textContent="0";
+    secondscore.textContent="0";
     let loto=document.querySelector(".main-loto");
     let premain=document.querySelector(".main-prev");
     premain.style.display='none';
     loto.style.display='block';
-    alert("Hello!");
+})
+document.querySelector("#accelerator").addEventListener("click",function(){
+    document.querySelector("#accelerator").style.display='none';
+    document.querySelector(".loto-info").style.display='block';
+    alert("abba");
 })
