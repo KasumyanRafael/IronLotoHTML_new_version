3e78872810823be55e6b1b19ca71ea2117b465ad
KasumyanRafael <100214646+KasumyanRafael@users.noreply.github.com>
2023-08-14 14:09:21 +0300

    Добавил класс WordArray

--- class.js
+++ class.js
@@ -29,3 +29,46 @@ class Word{
         return this.pictureshow;
     }
 }
+class WordArray{
+    wordmas=[];
+    Length=0;
+    count=0;
+    constructor(massiv,number){
+        count=number+1;
+        wordmas=this.ToWord(massiv);
+    }
+    ToWord(massiv){  //конвертация строкового массива в массив типа Word
+        var temp=new Array(massiv.Length);
+        for(var i=0;i<massiv.Length;i++){
+            temp[i]=new Word(massiv[i])
+        }
+        return temp;
+    }
+    GetExtract(wordmassiv){
+        result=new Array(count);
+        for(var i=0;i<this.count;i++){
+            result[i]=wordmassiv[i];
+            return result;
+        }
+    }
+    Antirepeat(wordmassiv,stringmassiv){
+        var min=0;
+        for(var i=stringmassiv.Length-1;i>=1;i--){
+            var max=i+1;
+            var j=Math.floor(Math.random() * (max - min)) + min;
+            var temp=wordmassiv[j];
+            wordmassiv[j]=wordmassiv[i];
+            wordmassiv[i]=temp;
+        }
+    }
+    Shuffling(){
+        var min=0;
+        for(var i=this.count-2;i>=1;i--){
+            var max=i+1;
+            var j=Math.floor(Math.random() * (max - min)) + min;
+            var temp=this.wordmas[j];
+            this.wordmas[j]=this.wordmas[i];
+            this.wordmas[i]=temp;
+        }
+    }
+}
